0e8426ffc2a295d8c10ec57786e9fb00f0e4b68f
Adit Sinsinwal <asinsinw@uwaterloo.ca>
2025-07-08 21:32:00 -0400

    Create data_quality_checks.sql

---
+++ data_quality_checks.sql
@@ -0,0 +1,22 @@
+-- Find missing coordinates in air_quality_measurements
+SELECT * FROM air_quality_measurements
+WHERE latitude IS NULL OR longitude IS NULL;
+
+-- Detect inconsistent country names between datasets
+SELECT DISTINCT a.country
+FROM air_quality_measurements a
+LEFT JOIN country_health_metrics h ON a.country = h.country
+WHERE h.country IS NULL;
+
+-- Check for negative or nonsensical pollution values
+SELECT * FROM air_quality_measurements
+WHERE value < 0;
+
+-- Ensure each country has at least one health record for the latest year
+SELECT country
+FROM country_demographics
+WHERE year = 2023
+EXCEPT
+SELECT country
+FROM country_health_metrics
+WHERE year = 2023;
